5454e8157068f6174d5ff34ba60b7fc58b944b16
deepak <deepakmpersonal@gmail.com>
2025-09-10 23:55:28 +0530

    hi

--- src/Components/loginPage.jsx
+++ src/Components/loginPage.jsx
@@ -1,75 +1,86 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../StyleSheets/loginPage.css";
 import { Link } from "react-router-dom";
-
-// Login Component 
+// login Component 
 const LoginPage = ({ loginType }) => {
 
     //Hooks declaration
-    const [rollNo, setRollNo] = useState("");
-    const [password, setPassword] = useState("");
+    const [rollNo, setRollNo] = useState();
+    const [password, setPassword] = useState("")
     const navigate = useNavigate();
-
-    // Handle submit 
+ 
+    // Handle submit................................................................................................... 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(password);
-        console.log(rollNo);
-    };
-
-    //Number check function 
+        e.preventDefault()
+        console.log(password)
+        console.log(rollNo)
+    }
+ 
+    //Number check function....................................................................................... 
     const checkNumberType = (value) => {
+
         return value.replace(/[^0-9]/g, "");
     };
-
-    //Function Declaration 
+ 
+    //Function declaration.................................................................................................
     const handleLogin = () => {
-        if (loginType === "Student") {
-            navigate("/Dashboard");
-        } else {
-            navigate("/FacultyDashboard");
-        }
-    };
+    if(loginType=="Student"){
+        navigate("/Dashboard")}
+    else{
+         navigate("/FacultyDashboard")
+    }    
+        //fetch path not defined so commented--------------------------------------------------------------------------------
+        // fetch("https://" , {
+        //     method:"POST",
+        //     headers: {"content-type":"application/json"},
+        //     body:JSON.stringify({
+        //         rollNo:rollNo,
+        //         password:password
+        //     })
+        // })
+    }
+
+
 
     return (
         <div className="login-page">
-            {/* 🔹 Heading at top */}
-            <h1 className="main-heading">Digital Attendance System</h1>
-
             <div className="login-container">
-                <h2>{loginType} Login</h2>  {/* Shows "Student Login" or "Faculty Login" */}
-
+                <h1>Login</h1>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="rollNo" className="input-label">{loginType} ID</label>
+
+                    <label htmlFor="rollNo" id="rollNoLabel" className="input-label">{loginType} ID</label>
                     <input
                         autoComplete="off"
                         id="rollNo"
-                        className="input-box"
+                        className="inpexut-box"
                         type="text"
                         value={rollNo}
-                        maxLength={11}
+                        max-length={11}
                         onChange={(e) => setRollNo(checkNumberType(e.target.value))}
-                        placeholder="Enter your ID No"
-                    />
+                        placeholder="Enter your ID No">
+                    </input>
 
-                    <label htmlFor="password" className="input-label">Password</label>
+                    <label htmlFor="password" id="password" className="input-label">Password</label>
                     <input
                         autoComplete="off"
+                        htmlFor="password"
                         id="password"
-                        placeholder="Enter Your Password"
                         className="input-box"
                         type="password"
                         onChange={(e) => setPassword(e.target.value)}
-                    />
+                    >
+                    </input>
+                    <button type="submit" onClick={handleLogin} className="Login-Button">Login</button>
+                    {/* <p>Want to change password ?<Link  to="/signUp">Change Password </Link></p> */}
 
-                    <button type="submit" onClick={handleLogin} className="Login-Button">
-                        Login
-                    </button>
-                </form>
+
+                </form >
             </div>
         </div>
-    );
-};
 
+
+    );
+}
 export default LoginPage;
